50db91bde96d5612405d3efe35a050dea7eb88c6
Lucian B <brodos@outlook.com>
2019-03-03 14:20:10 +0100

    added purgecss for production scripts and resized and optimized the images

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,38 @@
-const mix = require('laravel-mix');
-var tailwindcss = require('tailwindcss');
+let mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
+let glob = require("glob-all");
+let PurgecssPlugin = require("purgecss-webpack-plugin");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+class TailwindExtractor {
+  	static extract(content) {
+    	return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  	}
+}
 
 mix.js('./resources/js/app.js', 'public/js')
    .postCss('./resources/css/app.css', 'public/css', [
    		tailwindcss('./tailwind.js'),
    	])
    .version();
+
+if (mix.inProduction()) {
+  	mix.webpackConfig({
+    	plugins: [
+      		new PurgecssPlugin({
+	        	// Specify the locations of any files you want to scan for class names.
+	        	paths: glob.sync([
+	          		path.join(__dirname, "resources/views/**/*.blade.php"),
+	          		path.join(__dirname, "resources/js/**/*.vue"),
+	          		path.join(__dirname, "resources/js/*.js")
+	        	]),
+	        	extractors: [{
+	 	           	extractor: TailwindExtractor,
+
+	            	// Specify the file extensions to include when scanning for
+	            	// class names.
+	            	extensions: ["html", "js", "php", "vue"]
+	          	}]
+      		})
+    	]
+  	});
+}
